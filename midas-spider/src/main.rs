699cbd4b71978a7038ee9814d9e227fccaf57197
jerry <calljerryli@outlook.com>
2023-03-17 01:33:00 +0800

    midas-spider futures join_all

--- midas-spider/src/main.rs
+++ midas-spider/src/main.rs
@@ -7,7 +7,10 @@ async fn main() {
     let client = reqwest::Client::builder().build().unwrap();
 
     let index_code_list = midas_core::index_code::list().unwrap();
-    for index_code in index_code_list {
-        midas_spider::fetch_data(&index_code, &client).await
-    }
+
+    let fetch_data_future_iter = index_code_list
+        .iter()
+        .map(|index_code| midas_spider::fetch_data(index_code, &client));
+
+    futures::future::join_all(fetch_data_future_iter).await;
 }
