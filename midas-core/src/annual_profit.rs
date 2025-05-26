1e202cca3801a291f76bca517370936ba29a73c5
Jerry <calljerryli@outlook.com>
2025-05-26 21:32:45 +0800

    chore: rename variable from year_list to year_set for clarity

--- midas-core/src/annual_profit.rs
+++ midas-core/src/annual_profit.rs
@@ -2,12 +2,12 @@ use crate::*;
 use rayon::prelude::*;
 
 pub fn list(profit_list: &[model::Profit]) -> Vec<model::AnnualProfit> {
-    let year_list = profit_list
+    let year_set = profit_list
         .par_iter()
         .map(|item| item.date.split('-').next().unwrap().to_string())
         .collect::<std::collections::BTreeSet<String>>();
 
-    year_list
+    year_set
         .par_iter()
         .map(|year| {
             let mut iter = profit_list.iter().filter(|item| item.date.contains(year));
